6c69cce1967083bc6609f322ac03ceca6e007cf8
Peter Skrivanek <S00236650@atu.ie>
2024-03-20 13:00:51 +0000

    Added boss class

---
+++ OOD_Project/Boss.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOD_Project
+{
+    internal class Boss
+    {
+        public int BossID { get; set; }
+        public string BossName { get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
