e9d0a8de51bec2a8de6c04c1f36d49dc35ca5747
Peter Skrivanek <S00236650@atu.ie>
2024-04-24 11:38:50 +0100

    started work on the database

---
+++ DataManagement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOD_Project;
+
+namespace DataManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create DB model
+            GameData db = new GameData("DarkSoulsGames");
+
+            using(db)
+            {
+                // Create Games
+                Game ds1 = new Game("Dark Souls");
+                Game ds3 = new Game("Dark Souls III");
+
+                // Create Bosses
+                Boss AssylumDemon = new Boss("Assylum Demon", "Undead Assylum", 825,
+                    new List<string>() { "Fire", "Bleed" },
+                    2000, new List<string>() { "1 Humanity", "Big Pilgrim's Key", "Demon's Great Hammer" });
+                Boss BellGargoylse = new Boss("Bell Gargoyles", "Undead Parish", 1480,
+                    new List<string>() { "Fire", "Lightning" },
+                    10000, new List<string>() { "1 Twin Humanities", "Gargoyle's Halberd", "Gargoyle Shield", "Gargoyle Helm", "Gargoyle Tail Axe" });
+
+                // Add to DB and save
+
+            }
+        }
+    }
+}
--- OOD_Project/Boss.cs
+++ OOD_Project/Boss.cs
@@ -1,15 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OOD_Project
 {
-    internal class Boss
+    public class Game
+    {
+        public int GameID { get; set; }
+        public string GameName { get; set; }
+        public virtual List<Boss> Bosses { get; set; }
+
+        public Game(string name)
+        {
+            GameName = name;
+            Bosses = new List<Boss>();
+        }
+    }
+
+    public class Boss
     {
         public int BossID { get; set; }
         public string BossName { get; set; }
-        public List<string> Tags { get; set; }
+        public string Location { get; set; }
+        public int Health { get; set; }
+        public List<string> Weaknesses { get; set; }
+        public int Souls { get; set; }
+        public List<string> Drops { get; set; }
+
+        public Boss(string bossName, string location, int health, List<string> weaknesses, int souls, List<string> drops)
+        {
+            BossName = bossName;
+            Location = location;
+            Health = health;
+            Weaknesses = weaknesses;
+            Souls = souls;
+            Drops = drops;
+        }
+    }
+
+    public class GameData : DbContext
+    {
+        public GameData(string gameName) : base(gameName) { }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Boss> Bosses { get; set; }
+        
     }
 }
